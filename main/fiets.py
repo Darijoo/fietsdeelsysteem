a20962181934f99e4fcd08f97990a6e23d83a2cf
Darijoo <128596761+Darijoo@users.noreply.github.com>
2023-05-19 16:30:31 +0200

    dag 3

--- main/fiets.py
+++ main/fiets.py
@@ -1,12 +1,23 @@
 
 class Fiets:
-    def __init__(self, fiets_id, inUse=False):
-        self.inUse = False
+    def __init__(self, fiets_id, station=None, slot=None, in_use=False):
         self.fiets_id = fiets_id
+        self.station = station
+        self.slot = slot
+        self.in_use = in_use
 
-    def setInUse(self, inUse):
-        self.inUse = inUse
-        inUse = True
-        return inUse
+    def printFietsInfo(self, stations, username):
+        print(f"Fiets ID: {self.fiets_id}")
+        for station in stations:
+            for slot in station.slots:
+                if slot.bike == self:
+                    print(f"Station: {station.name}")
+                    print(f"Slot: {slot.slot_id}")
+        for user in username:
+            if user.hasBike == True:
+                print(f"Gebruiker: {user.username}")
+                break
+        else:
+            print("Gebruiker: Geen gebruiker")
     
 
--- main/gebruiker.py
+++ main/gebruiker.py
@@ -16,3 +16,19 @@ class Gebruiker:
     def voegGebruikerToe(self, username):
         username.append(self)
         print(f"De gebruiker {self.username} is toegevoegd.")
+
+    def giveBike(self, bike):
+        if self.hasBike == False:
+            self.hasBike = True
+            bike.in_use = True
+            print(f"De gebruiker {self.username} heeft nu een fiets.")
+        else: 
+            print(f"De gebruiker {self.username} heeft al een fiets.")
+
+    def returnBike(self, bike):
+        if self.hasBike == True:
+            self.hasBike = False
+            bike.in_use = False
+            print(f"De gebruiker {self.username} heeft nu geen fiets meer.")
+        else:
+            print(f"De gebruiker {self.username} heeft geen fiets om in te leveren.")
