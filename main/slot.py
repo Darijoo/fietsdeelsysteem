746e3f899cfc5429fee469f1a6ddcec18ce0398b
Darijoo <128596761+Darijoo@users.noreply.github.com>
2023-05-19 16:48:08 +0200

    dag 3

--- main/slot.py
+++ main/slot.py
@@ -8,3 +8,23 @@ class Slot: # slots in stations
 
     def remove_bike(self):
         self.bike = None
+
+    def printSlotInfo(self, stations, username):
+        print(f"Slot ID: {self.slot_id}")
+        for station in stations:
+            for slot in station.slots:
+                if slot.slot_id == self.slot_id:
+                    print(f"Station: {station.name}")
+                    break
+        else:
+            print("Station: Geen station")
+        if self.bike != None:
+            print(f"Fiets: {self.bike.fiets_id}")
+        else:
+            print("Fiets: Geen fiets")
+        for user in username:
+            if user.hasBike == True:
+                print(f"Gebruiker: {user.username}")
+                break
+        else:
+            print("Gebruiker: Geen gebruiker")
