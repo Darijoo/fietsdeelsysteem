ef0c82edc29b3c642664909aebd4cff2cb875eb7
Darijoo <128596761+Darijoo@users.noreply.github.com>
2023-05-31 23:15:55 +0200

    praktisch verbeterd

--- main/main.py
+++ main/main.py
@@ -5,10 +5,10 @@ from station import Station
 from fiets import Fiets
 from pick import pick
 
-bigbrain = Controller()
+bigbrain = Controller(numBikes=3620, numUsers=4200)
+users = bigbrain.gen.getUsers()
+gebruiker = Gebruiker(users, users)
 
-# station = Station("Example Station", "123", 10,"In gebruik")
-# station.printSystemInfo(stations, username)
 stations = bigbrain.gen.getStations()
 
 valid_responses = []
@@ -16,7 +16,8 @@ for i in range(13):
     valid_responses.append(i)
 
 username = input("Geef gebruiksernaam ")
-
+if username not in bigbrain.gen.getUsers():
+    bigbrain.gen.addUser(username)
 while True:
     print("Wat wil je doen?")
     print("1. fiets huren")
@@ -39,16 +40,19 @@ while True:
     match(response):
         case 1:
             bike = None
-            while(not bike):
-                print([station.name for station in stations])
-                station = bigbrain.getStationByName(input("Geef naam van station: "))
-                if not station:
-                    print("Kan station niet vinden. Check spelling")
+            while not bike:
+                for i, station in enumerate(stations):
+                    print(f"{i + 1}. {station.name}")
+                station_number = int(input("Geef het nummer van het station: "))
+                if station_number < 1 or station_number > len(stations):
+                    print("Ongeldig stationnummer. Probeer opnieuw.")
                     continue
+                station = stations[station_number - 1]
                 bike = bigbrain.getFreeBike(station)
                 if not bike:
-                    print("Geen fiets in dit station, geef een ander station")
+                    print("Geen fiets in dit station. Kies een ander station.")
                     continue
+
         case 2:
             bike = None
             while(not bike):
@@ -66,5 +70,12 @@ while True:
             if not station:
                 print("Kan station niet vinden. Check spelling")
                 continue
-            station.printStationInfo(station, username)
+            station.printStationInfo()
+
+        case 4:
+            user = Gebruiker(input("Geef gebruikersnaam: "))
+            if not user:
+                print("Kan gebruiker niet vinden. Check spelling")
+                continue
+            bigbrain.printUserInfo(user)
         
