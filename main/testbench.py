8244cdec099485b9a1d50206da236d4868657d36
Darijoo <128596761+Darijoo@users.noreply.github.com>
2023-05-31 23:16:10 +0200

    testbench aangemaakt om defs te testen

--- main/testbench.py
+++ main/testbench.py
@@ -1,3 +1,9 @@
-
+from controller import Controller
+from gebruiker import Gebruiker
 from station import Station
 from fiets import Fiets
+from pick import pick
+
+bigbrain = Controller()
+
+# bigbrain.printAllUsernames()
